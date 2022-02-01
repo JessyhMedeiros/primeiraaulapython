566f6a057a231b15ec9f6d7c019ced838be62dae
Jessica <Jessyholiveira2016@hotmail.com>
2022-01-31 22:30:30 -0300

    meu primeiro projeto

---
+++ main.py
@@ -0,0 +1,28 @@
+import math
+
+print("ola, mundo")
+
+valor8 = 2 * 1
+valor7 = 2 * 2
+valor6 = 2 * 3
+valor5 = 2 * 4
+valor4 = 2 * 5
+valor3 = 2 * 6
+valor2 = 2 * 7
+valor1 = 2 * 8
+valor = 2 * 9
+
+print(str(valor8))
+print(str(valor7))
+print(str(valor6))
+print(str(valor5))
+print(str(valor4))
+print(str(valor3))
+print(str(valor2))
+print(str(valor1))
+print(str(valor))
+
+a = 3 * 1
+b = 3 * 2
+c = 3 * 3
+print(f'o resultado é:{math.pow(3, 3)}')
